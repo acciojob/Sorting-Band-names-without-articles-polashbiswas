9923d445be202b179421afcca26476fd4ec92406
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-03-29 18:37:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
 //your code here
-const bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'Rolling Stones', 'Queen', 'Anthrax', 'Black Sabbath'];
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+touristSpots=touristSpots.sort();
+var ul=document.getElementById("#band");
+for(let i=0;i<touristSpots.length;i++){
+	let li=document.createElement("li");
+ul.appendChild(li);
 
-		function sortBands(bands) {
-			const articles = ['the', 'a', 'an'];
-			const sortedBands = bands.sort(function(a, b) {
-				const nameA = a.toLowerCase().replace(/^(the|an|a)\s+/, '');
-				const nameB = b.toLowerCase().replace(/^(the|an|a)\s+/, '');
-				if (nameA < nameB) {
-					return -1;
-				}
-				if (nameA > nameB) {
-					return 1;
-				}
-				return 0;
-			});
-			return sortedBands;
-		}
-
-		const sortedBands = sortBands(bandNames);
-
-		const bandList = document.getElementById("band");
-		sortedBands.forEach(function(band) {
-			const li = document.createElement("li");
-			li.appendChild(document.createTextNode(band));
-			bandList.appendChild(li);
-		});
+}
