fc93360e3937246fb3c19602bd14cfacc5e6913c
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-03-29 18:31:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+touristSpots=touristSpots.sort();
+var ul=document.getElementById("#band");
+for(let i=0;i<touristSpots.length;i++){
+	let li=document.createElement("li");
+ul.appendChild(li);
 
+}
